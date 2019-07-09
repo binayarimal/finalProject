3745d8f404c4469c16bedd524cc06eb11c5b6671
Binaya Rimal <rimal.binaya@gmail.com>
2019-07-08 22:29:32 -0700

    heroku deployed

--- client/src/components/items.js
+++ client/src/components/items.js
@@ -9,7 +9,7 @@ class Items extends Component {
       item :"",
       itemList:[],
       email:"",
-      endpoint : "http://localhost:5000"
+      endpoint : "https://binayarimal-bloc-shopping-cart.herokuapp.com/"
     }
   }
   setItemList(items){
